b856340bea74dd6c036520a70713a5dfaae15e8d
luosch <me@lsich.com>
2015-09-23 00:27:23 +0800

    20150923

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ leetcode solution [python, c++, java]
 
 total 285 (268+17) algorithms problem
 # progress	
-	python 70 / 285
-	cpp    0  / 285
-	java   0  / 285
+	python 71 (70+1) / 285
+	cpp    0  (0+0)  / 285
+	java   0  (0+0) / 285
---
+++ python/Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if root is None:
+            return None
+        
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        else:
+            if left:
+                return left
+            else:
+                return right
