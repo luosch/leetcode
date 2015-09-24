f866844564cf3d95e397610b46f83a9a469cfb5b
luosch <me@lsich.com>
2015-09-24 12:39:57 +0800

    20150924

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ leetcode solution [python, c++, java]
 
 total 285 algorithms problem
 # progress	
-	python 72 / 285
+	python 73 / 285
 	cpp    0  / 285
 	java   0  / 285
---
+++ python/Rotate Array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverse(self, nums, left, right):
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+    def rotate(self, nums, k):
+        k = k % len(nums)
+        self.reverse(nums, 0, len(nums) - k - 1)
+        self.reverse(nums, len(nums) - k, len(nums) - 1)
+        self.reverse(nums, 0, len(nums) - 1)
