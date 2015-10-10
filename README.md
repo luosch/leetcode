571390d5be0138d663d9333f2a44d6904dc3b23c
luosch <me@lsich.com>
2015-10-10 10:52:04 +0800

    20151010

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ leetcode solution [python, cpp, java]
 
 total 273+17 algorithms problem
 # progress	
-	python 93+2 / 273+17
+	python 94+2 / 273+17
 	cpp    0    / 273+17
 	java   0    / 273+17
---
+++ python/Rectangle Area.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        overlap = max(min(C, G) - max(A, E), 0) * max(min(D, H)-max(B, F), 0)
+        union_area = (A - C) * (B - D) + (E - G) * (F - H)
+        return union_area - overlap
