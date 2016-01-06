b78dfdbd61c42bb51a81f7015df53bcdcaf0ecfc
luosch <me@lsich.com>
2016-01-06 15:33:38 +0800

    20160106 :muscle:

---
+++ python/Shortest Palindrome.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def shortestPalindrome(self, s):
+        rev = s[::-1]
+        l = s + '#' + rev
+        p = [0] * len(l)
+    
+        for i in range(1, len(l)):
+            j = p[i - 1]
+            while j > 0 and l[i] != l[j]:
+                j = p[j - 1]
+            p[i] = j + (l[i] == l[j])
+    
+        return rev[:len(s) - p[-1]] + s
