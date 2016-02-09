5017c836769f6ba44ec0f10be9e2a6eab0823efd
luosch <me@lsich.com>
2016-02-09 11:17:07 +0800

    20160209 :muscle:

--- python/Strobogrammatic Number.py
+++ python/Strobogrammatic Number.py
@@ -1,5 +1,16 @@
 class Solution(object):
     def isStrobogrammatic(self, num):
+        m = {'0': '0', '1': '1', '8': '8', '6': '9', '9': '6'}
+        n = len(num)
+        left, right = 0, n - 1
+        while left <= right:
+            if num[left] not in m or m[num[left]] != num[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+        
+    def isStrobogrammatic_slow(self, num):
         check = []
         for c in num:
             if c in '018':
